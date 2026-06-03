c5a248d6de24d83648b3797176c1abc34e39e2b7
unknown <rishisharma.bca25@satyug.edu.in>
2026-06-04 00:40:32 +0530

    setup security and quality

--- eslint.config.js
+++ eslint.config.js
@@ -17,5 +17,13 @@ export default defineConfig([
       globals: globals.browser,
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-empty': 'warn',
+      'no-useless-escape': 'warn',
+      'no-useless-assignment': 'warn',
+      'react-refresh/only-export-components': 'warn',
+      'react-hooks/set-state-in-effect': 'off',
+    }
   },
 ])
